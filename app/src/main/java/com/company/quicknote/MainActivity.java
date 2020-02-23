2823b91ed3f57af980d076839e8310cffae30e86
tudor199 <tuddor144@gmail.com>
2020-02-23 17:21:14 +0200

    added list

--- app/src/main/java/com/company/quicknote/MainActivity.java
+++ app/src/main/java/com/company/quicknote/MainActivity.java
@@ -3,6 +3,8 @@ package com.company.quicknote;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -16,11 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.note_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                
+                adapter.setNotes(notes);
             }
         });
     }
---
+++ app/src/main/java/com/company/quicknote/NoteAdapter.java
@@ -0,0 +1,56 @@
+package com.company.quicknote;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+    List<Note> notes = new ArrayList<>();
+
+    class NoteHolder extends RecyclerView.ViewHolder{
+        private TextView title;
+        private TextView description;
+        private TextView priority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+            description = itemView.findViewById(R.id.description);
+            priority = itemView.findViewById(R.id.priority);
+        }
+    }
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_note, parent, false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        Note note = notes.get(position);
+        holder.title.setText(note.getTitle());
+        holder.description.setText(note.getDescription());
+        holder.priority.setText(String.valueOf(note.getPriority()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+}
+
