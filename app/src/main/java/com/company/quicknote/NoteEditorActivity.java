4eadb1646ae593ad85e1f746f90f4b3b7e83418a
tudor199 <tuddor144@gmail.com>
2020-02-25 15:15:01 +0200

    Change priority number picker to progressbar

--- app/src/main/java/com/company/quicknote/NoteEditorActivity.java
+++ app/src/main/java/com/company/quicknote/NoteEditorActivity.java
@@ -2,12 +2,14 @@ package com.company.quicknote;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.EditText;
-import android.widget.NumberPicker;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,42 +20,81 @@ public class NoteEditorActivity extends AppCompatActivity {
 
     private EditText texTitle;
     private EditText textDescription;
-    private NumberPicker pickerPriority;
+    private EditText textPriority;
+    private SeekBar seekPriority;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_editor);
 
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+
         texTitle = findViewById(R.id.title);
         textDescription = findViewById(R.id.description);
-        pickerPriority = findViewById(R.id.picker_priority);
+        textPriority = findViewById(R.id.priority_text);
+        seekPriority = findViewById(R.id.priority_seekbar);
 
-        pickerPriority.setMinValue(1);
-        pickerPriority.setMaxValue(10);
+        seekPriority.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textPriority.setText(String.valueOf(progress));
+                textPriority.setSelection(textPriority.getEditableText().toString().length());
+            }
 
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        textPriority.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                try {
+                    seekPriority.setProgress(Integer.parseInt(s.toString()));
+                } catch (Exception e) {
+                    seekPriority.setProgress(0);
+                }
+            }
+        });
+
+        seekPriority.setMax(10);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
         Intent intent = getIntent();
         id = intent.getIntExtra(Constants.KEY_ID, -1);
         if (id == -1) {
             setTitle("Add Note");
-            pickerPriority.setValue(5);
+            seekPriority.setProgress(5);
             texTitle.requestFocus();
         } else {
             setTitle("Edit Note");
             texTitle.setText(intent.getStringExtra(Constants.KEY_TITLE));
             textDescription.setText(intent.getStringExtra(Constants.KEY_DESCRIPTION));
-            pickerPriority.setValue(intent.getIntExtra(Constants.KEY_PRIORITY, 1));
+            seekPriority.setProgress(intent.getIntExtra(Constants.KEY_PRIORITY, 1));
             textDescription.requestFocus();
         }
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
     }
 
     private void saveNote() {
         String title = texTitle.getText().toString();
         String description = textDescription.getText().toString();
-        int priority = pickerPriority.getValue();
+        int priority = seekPriority.getProgress();
 
         if (title.trim().isEmpty()) {
             Toast.makeText(this, "Title cannot be empty", Toast.LENGTH_SHORT).show();
